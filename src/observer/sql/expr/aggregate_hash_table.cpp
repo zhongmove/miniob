9735c3532a14d49de0af0748a3759d14bc016ce7
Wang Zhongmove <805526785@qq.com>
2024-07-16 09:26:28 +0000

    aggregation

--- src/observer/sql/expr/aggregate_hash_table.cpp
+++ src/observer/sql/expr/aggregate_hash_table.cpp
@@ -8,139 +8,239 @@ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
-#include <algorithm>
-#include "common/log/log.h"
-#include "sql/operator/aggregate_vec_physical_operator.h"
-#include "sql/expr/aggregate_state.h"
-#include "sql/expr/expression_tuple.h"
-#include "sql/expr/composite_tuple.h"
+#include "sql/expr/aggregate_hash_table.h"
 
-using namespace std;
-using namespace common;
+// ----------------------------------StandardAggregateHashTable------------------
 
-AggregateVecPhysicalOperator::AggregateVecPhysicalOperator(vector<Expression *> &&expressions)
+RC StandardAggregateHashTable::add_chunk(Chunk &groups_chunk, Chunk &aggrs_chunk)
 {
-  aggregate_expressions_ = std::move(expressions);
-  value_expressions_.reserve(aggregate_expressions_.size());
-
-  ranges::for_each(aggregate_expressions_, [this](Expression *expr) {
-    auto *      aggregate_expr = static_cast<AggregateExpr *>(expr);
-    Expression *child_expr     = aggregate_expr->child().get();
-    ASSERT(child_expr != nullptr, "aggregation expression must have a child expression");
-    value_expressions_.emplace_back(child_expr);
-  });
-
-  for (size_t i = 0; i < aggregate_expressions_.size(); i++) {
-    auto &expr = aggregate_expressions_[i];
-    ASSERT(expr->type() == ExprType::AGGREGATION, "expected an aggregation expression");
-    auto *aggregate_expr = static_cast<AggregateExpr *>(expr);
-
-    if (aggregate_expr->aggregate_type() == AggregateExpr::Type::SUM) {
-      if (aggregate_expr->value_type() == AttrType::INTS) {
-        void *aggr_value                     = malloc(sizeof(SumState<int>));
-        ((SumState<int> *)aggr_value)->value = 0;
-        aggr_values_.insert(aggr_value);
-        output_chunk_.add_column(make_unique<Column>(AttrType::INTS, sizeof(int)), i);
-      } else if (aggregate_expr->value_type() == AttrType::FLOATS) {
-        void *aggr_value                       = malloc(sizeof(SumState<float>));
-        ((SumState<float> *)aggr_value)->value = 0;
-        aggr_values_.insert(aggr_value);
-        output_chunk_.add_column(make_unique<Column>(AttrType::FLOATS, sizeof(float)), i);
-      }
-    } else {
-      ASSERT(false, "not supported aggregation type");
-    }
-  }
+  // your code here
+  exit(-1);
 }
 
-RC AggregateVecPhysicalOperator::open(Trx *trx)
+void StandardAggregateHashTable::Scanner::open_scan()
 {
-  ASSERT(children_.size() == 1, "group by operator only support one child, but got %d", children_.size());
+  it_  = static_cast<StandardAggregateHashTable *>(hash_table_)->begin();
+  end_ = static_cast<StandardAggregateHashTable *>(hash_table_)->end();
+}
 
-  PhysicalOperator &child = *children_[0];
-  RC                rc    = child.open(trx);
-  if (OB_FAIL(rc)) {
-    LOG_INFO("failed to open child operator. rc=%s", strrc(rc));
-    return rc;
+RC StandardAggregateHashTable::Scanner::next(Chunk &output_chunk)
+{
+  if (it_ == end_) {
+    return RC::RECORD_EOF;
+  }
+  while (it_ != end_ && output_chunk.rows() <= output_chunk.capacity()) {
+    auto &group_by_values = it_->first;
+    auto &aggrs           = it_->second;
+    for (int i = 0; i < output_chunk.column_num(); i++) {
+      auto col_idx = output_chunk.column_ids(i);
+      if (col_idx >= static_cast<int>(group_by_values.size())) {
+        output_chunk.column(i).append_one((char *)aggrs[col_idx - group_by_values.size()].data());
+      } else {
+        output_chunk.column(i).append_one((char *)group_by_values[col_idx].data());
+      }
+    }
+    it_++;
+  }
+  if (it_ == end_) {
+    return RC::SUCCESS;
   }
 
-  while (OB_SUCC(rc = child.next(chunk_))) {
-    for (size_t aggr_idx = 0; aggr_idx < aggregate_expressions_.size(); aggr_idx++) {
-      Column column;
-      value_expressions_[aggr_idx]->get_column(chunk_, column);
-      ASSERT(aggregate_expressions_[aggr_idx]->type() == ExprType::AGGREGATION, "expect aggregate expression");
-      auto *aggregate_expr = static_cast<AggregateExpr *>(aggregate_expressions_[aggr_idx]);
-      if (aggregate_expr->aggregate_type() == AggregateExpr::Type::SUM) {
-        if (aggregate_expr->value_type() == AttrType::INTS) {
-          update_aggregate_state<SumState<int>, int>(aggr_values_.at(aggr_idx), column);
-        } else if (aggregate_expr->value_type() == AttrType::FLOATS) {
-          update_aggregate_state<SumState<float>, float>(aggr_values_.at(aggr_idx), column);
-        } else {
-          ASSERT(false, "not supported value type");
-        }
-      } else {
-        ASSERT(false, "not supported aggregation type");
+  return RC::SUCCESS;
+}
+
+size_t StandardAggregateHashTable::VectorHash::operator()(const vector<Value> &vec) const
+{
+  size_t hash = 0;
+  for (const auto &elem : vec) {
+    hash ^= std::hash<string>()(elem.to_string());
+  }
+  return hash;
+}
+
+bool StandardAggregateHashTable::VectorEqual::operator()(const vector<Value> &lhs, const vector<Value> &rhs) const
+{
+  if (lhs.size() != rhs.size()) {
+    return false;
+  }
+  for (size_t i = 0; i < lhs.size(); ++i) {
+    if (rhs[i].compare(lhs[i]) != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// ----------------------------------LinearProbingAggregateHashTable------------------
+#ifdef USE_SIMD
+template <typename V>
+RC LinearProbingAggregateHashTable<V>::add_chunk(Chunk &group_chunk, Chunk &aggr_chunk)
+{
+  if (group_chunk.column_num() != 1 || aggr_chunk.column_num() != 1) {
+    LOG_WARN("group_chunk and aggr_chunk size must be 1.");
+    return RC::INVALID_ARGUMENT;
+  }
+  if (group_chunk.rows() != aggr_chunk.rows()) {
+    LOG_WARN("group_chunk and aggr _chunk rows must be equal.");
+    return RC::INVALID_ARGUMENT;
+  }
+  add_batch((int *)group_chunk.column(0).data(), (V *)aggr_chunk.column(0).data(), group_chunk.rows());
+  return RC::SUCCESS;
+}
+
+template <typename V>
+void LinearProbingAggregateHashTable<V>::Scanner::open_scan()
+{
+  capacity_   = static_cast<LinearProbingAggregateHashTable *>(hash_table_)->capacity();
+  size_       = static_cast<LinearProbingAggregateHashTable *>(hash_table_)->size();
+  scan_pos_   = 0;
+  scan_count_ = 0;
+}
+
+template <typename V>
+RC LinearProbingAggregateHashTable<V>::Scanner::next(Chunk &output_chunk)
+{
+  if (scan_pos_ >= capacity_ || scan_count_ >= size_) {
+    return RC::RECORD_EOF;
+  }
+  auto linear_probing_hash_table = static_cast<LinearProbingAggregateHashTable *>(hash_table_);
+  while (scan_pos_ < capacity_ && scan_count_ < size_ && output_chunk.rows() <= output_chunk.capacity()) {
+    int key;
+    V   value;
+    RC  rc = linear_probing_hash_table->iter_get(scan_pos_, key, value);
+    if (rc == RC::SUCCESS) {
+      output_chunk.column(0).append_one((char *)&key);
+      output_chunk.column(1).append_one((char *)&value);
+      scan_count_++;
+    }
+    scan_pos_++;
+  }
+  return RC::SUCCESS;
+}
+
+template <typename V>
+void LinearProbingAggregateHashTable<V>::Scanner::close_scan()
+{
+  capacity_   = -1;
+  size_       = -1;
+  scan_pos_   = -1;
+  scan_count_ = 0;
+}
+
+template <typename V>
+RC LinearProbingAggregateHashTable<V>::get(int key, V &value)
+{
+  RC  rc          = RC::SUCCESS;
+  int index       = (key % capacity_ + capacity_) % capacity_;
+  int iterate_cnt = 0;
+  while (true) {
+    if (keys_[index] == EMPTY_KEY) {
+      rc = RC::NOT_EXIST;
+      break;
+    } else if (keys_[index] == key) {
+      value = values_[index];
+      break;
+    } else {
+      index += 1;
+      index %= capacity_;
+      iterate_cnt++;
+      if (iterate_cnt > capacity_) {
+        rc = RC::NOT_EXIST;
+        break;
       }
     }
   }
-
-  if (rc == RC::RECORD_EOF) {
-    rc = RC::SUCCESS;
-  }
-
   return rc;
 }
-template <class STATE, typename T>
-void AggregateVecPhysicalOperator::update_aggregate_state(void *state, const Column &column)
+
+template <typename V>
+RC LinearProbingAggregateHashTable<V>::iter_get(int pos, int &key, V &value)
 {
-  STATE *state_ptr = reinterpret_cast<STATE *>(state);
-  T *    data      = (T *)column.data();
-  state_ptr->update(data, column.count());
+  RC rc = RC::SUCCESS;
+  if (keys_[pos] == LinearProbingAggregateHashTable<V>::EMPTY_KEY) {
+    rc = RC::NOT_EXIST;
+  } else {
+    key   = keys_[pos];
+    value = values_[pos];
+  }
+  return rc;
 }
 
-RC AggregateVecPhysicalOperator::next(Chunk &chunk)
+template <typename V>
+void LinearProbingAggregateHashTable<V>::aggregate(V *value, V value_to_aggregate)
 {
-  int col_num = chunk.column_num();
-  for(int col_id =0 ;col_id<col_num ;col_id++)
-  {
+  if (aggregate_type_ == AggregateExpr::Type::SUM) {
+    *value += value_to_aggregate;
+  } else {
+    ASSERT(false, "unsupported aggregate type");
+  }
+}
 
-    if (chunk.column_ptr(col_id)->attr_type()==AttrType::INTS)
-    {
-      unique_ptr<Column> column1 = make_unique<Column>(AttrType::INTS, 4);
-      output_chunk_.add_column(std::move(column1),col_id);
-      int col_data = 0;
-      for (int row_id = 0; row_id < chunk.column_ptr(col_id)->count(); row_id++)
-      {
-        Value part_value = chunk.get_value(row_id,col_id);
-        col_data += part_value.get_int();
-        // char* data = (char*)malloc(sizeof())
-        char * data = (char*)&col_data;
-        output_chunk_.column_ptr(col_id)->append_one(data);
-        /* code */
-      }
+template <typename V>
+void LinearProbingAggregateHashTable<V>::resize()
+{
+  capacity_ *= 2;
+  std::vector<int> new_keys(capacity_);
+  std::vector<V>   new_values(capacity_);
 
-    }
-    else if(chunk.column_ptr(col_id)->attr_type()==AttrType::FLOATS)
-    {
-      unique_ptr<Column> column1 = make_unique<Column>(AttrType::FLOATS,sizeof(float));
-      output_chunk_.add_column(std::move(column1),col_id);
-      float col_data = 0;
-      for (int row_id = 0; row_id < chunk.column_ptr(col_id)->count(); row_id++)
-      {
-        Value part_value = chunk.get_value(row_id,col_id);
-        col_data += part_value.get_float();
-        char * data = (char*)&col_data;
-        output_chunk_.column_ptr(col_id)->append_one(data);
-        /* code */
+  for (size_t i = 0; i < keys_.size(); i++) {
+    auto &key   = keys_[i];
+    auto &value = values_[i];
+    if (key != EMPTY_KEY) {
+      int index = (key % capacity_ + capacity_) % capacity_;
+      while (new_keys[index] != EMPTY_KEY) {
+        index = (index + 1) % capacity_;
       }
+      new_keys[index]   = key;
+      new_values[index] = value;
     }
   }
-  return RC::SUCCESS;
+
+  keys_   = std::move(new_keys);
+  values_ = std::move(new_values);
 }
 
-RC AggregateVecPhysicalOperator::close()
+template <typename V>
+void LinearProbingAggregateHashTable<V>::resize_if_need()
 {
-  children_[0]->close();
-  LOG_INFO("close group by operator");
-  return RC::SUCCESS;
+  if (size_ >= capacity_ / 2) {
+    resize();
+  }
 }
+
+template <typename V>
+void LinearProbingAggregateHashTable<V>::add_batch(int *input_keys, V *input_values, int len)
+{
+  // your code here
+  exit(-1);
+
+  // inv (invalid) 表示是否有效，inv[i] = -1 表示有效，inv[i] = 0 表示无效。
+  // key[SIMD_WIDTH],value[SIMD_WIDTH] 表示当前循环中处理的键值对。
+  // off (offset) 表示线性探测冲突时的偏移量，key[i] 每次遇到冲突键，则off[i]++，如果key[i] 已经完成聚合，则off[i] = 0，
+  // i = 0 表示selective load 的起始位置。
+  // inv 全部初始化为 -1
+  // off 全部初始化为 0
+
+  // for (; i + SIMD_WIDTH <= len;) {
+    // 1: 根据 `inv` 变量的值，从 `input_keys` 中 `selective load` `SIMD_WIDTH` 个不同的输入键值对。
+    // 2. 计算 i += |inv|, `|inv|` 表示 `inv` 中有效的个数 
+    // 3. 计算 hash 值，
+    // 4. 根据聚合类型（目前只支持 sum），在哈希表中更新聚合结果。如果本次循环，没有找到key[i] 在哈希表中的位置，则不更新聚合结果。
+    // 5. gather 操作，根据 hash 值将 keys_ 的 gather 结果写入 table_key 中。
+    // 6. 更新 inv 和 off。如果本次循环key[i] 聚合完成，则inv[i]=-1，表示该位置在下次循环中读取新的键值对。
+    // 如果本次循环 key[i] 未在哈希表中聚合完成（table_key[i] != key[i]），则inv[i] = 0，表示该位置在下次循环中不需要读取新的键值对。
+    // 如果本次循环中，key[i]聚合完成，则off[i] 更新为 0，表示线性探测偏移量为 0，key[i] 未完成聚合，则off[i]++,表示线性探测偏移量加 1。
+  // }
+  //7. 通过标量线性探测，处理剩余键值对
+
+  // resize_if_need();
+}
+
+template <typename V>
+const int LinearProbingAggregateHashTable<V>::EMPTY_KEY = 0xffffffff;
+template <typename V>
+const int LinearProbingAggregateHashTable<V>::DEFAULT_CAPACITY = 16384;
+
+template class LinearProbingAggregateHashTable<int>;
+template class LinearProbingAggregateHashTable<float>;
+#endif
